ac676edbd0f963deaa177e5798ae85771af73585
Sudhanshu tiwari <sudhanshutiwari264@gmail.com>
2024-05-28 16:29:03 +0530

    added the edit  profile feature

--- app.js
+++ app.js
@@ -36,6 +36,17 @@ app.get(`/delete/:userid`,async (req,res)=>{
   res.redirect(`/read`);
 });
 
+app.get('/update/:userid',async (req, res)=>{
+  let updateonUser =  await userModel.findById(req.params.userid)
+  res.render("edit_user",{user:updateonUser})
+});
+
+app.post(`/save_details/:userid`,async (req, res)=>{
+  let {name,email, image} = req.body;
+  let user = await userModel.findOneAndUpdate({_id:req.params.userid},{name,email,image}, {new:true});
+  res.redirect('/read')
+});
+
 app.listen(3000,(err)=>{
   if(err) console.error(err);
   else console.log(`Khada hui aaj bhi wahi`);
